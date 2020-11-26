4578119d40eb7fd8f3aae8deb033941198d17c5d
Андрей <malyavin.andrey231@yandex.ru>
2020-11-26 20:13:21 +0300

    0.06

--- Hi'.py
+++ Hi'.py
@@ -1,18 +1,16 @@
-import sys
-
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QPoint
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QMainWindow
 from PyQt5.QtGui import QPainter, QColor, QBrush
 import random
+import sys
 from PyQt5 import uic
-from UI import Ui_MainWindow
 
 
-class MyWidget(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.repaint)
 
     def paintEvent(self, e):
@@ -26,6 +24,7 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         col.setNamedColor('#d4d4d4')
         self.qp.setPen(col)
 
+        self.qp.setBrush(QColor(255, 255, 0))
         self.qp.setBrush(QColor(random.randint(0, 255),
                                 random.randint(0, 255),
                                 random.randint(0, 255)))
